24b76f1b4b1344ea6395b9325a305c8a62f91c00
Sami Laubo <slaubo99@gmail.com>
2023-05-31 11:21:37 +0200

    Update eval_panoptic_bev_nuscenes.sh

--- scripts/eval_panoptic_bev_nuscenes.sh
+++ scripts/eval_panoptic_bev_nuscenes.sh
@@ -1,12 +1,12 @@
 module load gcc/8.4.0-cuda python/3.7.7 cuda/11.1.1
 
 CUDA_VISIBLE_DEVICES=0\
-python3 -m torch.distributed.launch --nproc_per_node=1 --master_addr={IP ADDR} --master_port={PORT NUM} eval_panoptic_bev.py \
-                                    --run_name={NAME OF THE RUN} \
-                                    --project_root_dir={PATH OF THE TOP panoptic_bev FOLDER} \
-                                    --seam_root_dir={PATH OF THE NUSCENES SEAMLESS DATASET} \
-                                    --dataset_root_dir={PATH OF THE NUSCENES DATASET} \
+/home/laubo/DLAV/venv/panoptic_bev/bin/python -m torch.distributed.launch --nproc_per_node=1 --master_addr=$(hostname -I | cut -d ' ' -f 1) --master_port=$(python /home/laubo/DLAV/PanopticBEV/scripts/get_port.py) /home/laubo/DLAV/PanopticBEV/scripts/eval_panoptic_bev.py \
+                                    --run_name=eval_1epoch \
+                                    --project_root_dir=/home/laubo/DLAV/PanopticBEV \
+                                    --seam_root_dir=/home/laubo/DLAV/nuscenes/nuScenes_panopticbev\ \
+                                    --dataset_root_dir=/work/scitas-share/datasets/Vita/civil-459/NuScenes_full/US \
                                     --mode=test \
                                     --test_dataset=nuScenes \
-                                    --resume={PATH OF THE SAVED PTH MODEL TO USE FOR INFERENCE} \
+                                    --resume=/home/laubo/DLAV/PanopticBEV/experiments/bev_train_run12/saved_models/model_best.pth \
                                     --config=nuscenes.ini \
